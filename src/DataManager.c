d801a23cadf824c2cfc274163572f4ef8ea1e3e3
Julian Medrano <julian.medrano6@gmail.com>
2015-09-27 05:12:36 -0500

    Added getters and setter to DataManager.

---
+++ src/DataManager.c
@@ -0,0 +1,116 @@
+#include <pebble.h>
+#include "DataManager.h"
+
+  void setHeart(int heart)
+  {
+    am.heart= heart;
+  +
+    +
+  }
+  int getheart()
+  {
+    return am.heart;
+  }
+
+void setThirstExpire(time_t thirstExpire)
+  {
+    am.thirstExpire = thirstExpire;
+  
+  }
+  time_t getthirstExpire()
+  {
+    return am.thirstExpire;
+  }
+
+void setFunExpire(time_t funExpire)
+  {
+    am.funExpire = funExpire;
+  
+  }
+  time_t getfunExpire()
+  {
+    return am.funExpire;
+  }
+
+void setHungryExpire(time_t hungryExpire)
+  {
+    am.hungryExpire = hungryExpire;
+  
+  }
+  time_t gethungryExpire()
+  {
+    return am.hungryExpire;
+  }
+
+void setPlayExpire(time_t playExpire)
+  {
+    am.playExpire = playExpire;
+  
+  }
+  time_t getplayExpire()
+  {
+    return am.playExpire;
+  }
+/*
+subtractHeart will take in the amount of hearts lost and subtract by how much the current hearts the amigo has
+then it will update the expired time, and send  the new expired interval
+*/
+void subtractHeart(int heartLost)
+  {
+  am.heart -= heartLost; /* reduces the number of hearts from the heartlost  */
+  
+   time_t currentTime = time(NULL);
+    
+    int d = currentTime- am.thirstExpire;
+    int l = am.thirstExpire;
+    am.thirstExpire += ( d/l + (d % l == 0 ? 0 :1 )) * l; /* this will update thristExpire */
+    
+     d = currentTime - am.funExpire;
+     l = am.funExpire;
+    am.funExpire += ( d/l + (d % l == 0 ? 0 :1 )) * l; /* this will update funExpire */
+    
+    
+     d = currentTime - am.hungryExpire;
+     l = am.hungryExpire;
+    am.hungryExpire += ( d/l + (d % l == 0 ? 0 :1 )) * l; /* this will update hungryExpire */
+    
+    
+     d = currentTime - am.playExpire;
+     l = am.playExpire;
+    am.playExpire += ( d/l + (d % l == 0 ? 0 :1 )) * l; /* this will update playExpire */
+    
+   
+} 
+  
+
+/*
+heartLost will return how many hearts the amigo lost in the application
+*/
+
+int heartLost(time_t* thrirstExpire,time_t* funExpire,time_t* hungryExpire)
+  {
+    int heartLost = 0;
+    time_t currentTime = time(NULL);
+    heartLost += timesExpired(&currentTime, &am.thirstExpire, THRIST_EXPIRE);
+    heartLost += timesExpired(&currentTime, &am.funExpire, FUN_EXPIRE);
+    heartLost += timesExpired(&currentTime, &am.hungryExpire, HUNGRY_EXPIRE);
+    heartLost += timesExpired(&currentTime, &am.playExpire, PLAY_EXPIRE);
+    
+  return heartLost;
+  }
+
+/*
+timesExpired will return the an int for the amount of times the amigo expired for a given metrix
+if it didn't expire then it wuill return 0
+*/
+int timesExpired(time_t* currentTime, time_t* expireTime, int length) 
+  {
+    if(*currentTime > *expireTime)
+      {
+        return (*expireTime - *currentTime) / length + 1;
+      }
+    else
+      {
+        return 0;
+      }
+  }
---
+++ src/DataManager.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <pebble.h>
+#include <time.h>
+  
+#define THRIST_EXPIRE 60
+#define FUN_EXPIRE 120
+#define PLAY_EXPIRE 180
+#define HUNGRY_EXPIRE 240
+
+
+  /*
+  The Amigo struct is to define the characteristics of the Amigo, in the pebble application
+  Encluding heart, thirstExpire,funExpire, hungryExpire, and user_NameInput
+  */
+  struct Amigo 
+ {
+    int heart;
+    time_t thirstExpire;
+    time_t funExpire;
+    time_t hungryExpire;
+    time_t playExpire;
+    char name[50];
+ };
+
+struct Amigo am;
+
+void setHeart(int heart);
+int getHeart();
+
+time_t setThirstExpire(time_t thirstExpire);
+time_t getthirstExpire();
+
+time_t setFunExpire(time_t funExpire);
+time_t getfunExpire();
+
+time_t setHungryExpire(time_t hungryExpire);
+time_t gethungryExpire();
+
+time_t setPlayExpire(time_t playExpire);
+time_t getplayExpire();
+
+
+/*
+timesExpired will return the an int for the amount of times the amigo expired
+if it didn't expire then it wuill return 0
+*/
+
+int timesExpired(time_t* currrentTime, time_t* expireTime, int length); 
+  
+/*
+heartLost will return how many hearts the amigo lost in the application
+*/
+
+int heartLost(time_t* thrirstExpire,time_t* funExpire,time_t* hungryExpire);
+
+/*
+subtractHeart will take in the amount of hearts lost and subtract by how much the current hearts the amigo has
+then it will update the expired time, and send  the new expired interval
+*/
+void subtractHeart(int heartLost);
+
+
